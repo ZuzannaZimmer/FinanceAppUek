2e900c308cf87d95c755a6dc89f51e708f6dcb5b
Zuzanna Zimmer <114940033+ZuzannaZimmer@users.noreply.github.com>
2025-04-10 08:37:13 +0200

    Update firebase.js

--- projekt/src/firebase.js
+++ projekt/src/firebase.js
@@ -4,13 +4,7 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBvzkF5T6wEwBopwbBoCUAG4nmMrew_C5M",
-  authDomain: "financeappuek.firebaseapp.com",
-  databaseURL: "https://financeappuek-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "financeappuek",
-  storageBucket: "financeappuek.firebasestorage.app",
-  messagingSenderId: "1090939964421",
-  appId: "1:1090939964421:web:37f1c58825515301ee206c"
+  
   };
 
 const app = initializeApp(firebaseConfig);
