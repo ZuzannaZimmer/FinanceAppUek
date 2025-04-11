98ed59b55e2016881dde15a796c67547b6d03095
BartlomiejRegula <bartek.br2003@gmail.com>
2025-04-11 13:12:53 +0200

    pages poprawione

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: 'FinanceAppUek',
+  base: '/FinanceAppUek/',
   plugins: [
     react(),
     VitePWA({
